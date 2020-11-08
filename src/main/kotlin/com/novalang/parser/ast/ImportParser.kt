a6ce38a88579da3b702ca9e096ec7532b04010a4
Braden Steffaniak <BradenSteffaniak@gmail.com>
2020-11-08 08:45:05 -0500

    Convert ImportParser to Pipeline

--- src/main/kotlin/com/novalang/parser/ast/ImportParser.kt
+++ src/main/kotlin/com/novalang/parser/ast/ImportParser.kt
@@ -1,88 +1,59 @@
 package com.novalang.parser.ast
 
-import com.novalang.CompileError
 import com.novalang.ast.Import
 import com.novalang.parser.Dispatcher
+import com.novalang.parser.Pipeline
 import com.novalang.parser.State
-import com.novalang.parser.actions.DispatcherAction
-import com.novalang.parser.TokenData
 import com.novalang.parser.TokenType
+import com.novalang.parser.actions.DispatcherAction
 import com.novalang.parser.actions.FileParseAction
 import com.novalang.replace
 
 class ImportParser(dispatcher: Dispatcher) : Reducer(dispatcher) {
   override fun reduce(state: State, action: DispatcherAction): State {
     return when (action) {
-      is FileParseAction -> parseFile(state, action.tokenData)
+      is FileParseAction -> parseFile().run(dispatcher, state, action.tokenData)
       else -> state
     }
   }
 
-  private fun parseFile(state: State, tokenData: TokenData): State {
-    if (tokenData.tokens.unconsumed[0].type == TokenType.IMPORT) {
-      if (tokenData.tokens.unconsumed.size > 2) {
-        tokenData.tokens.consumeAll()
+  private fun parseFile(): Pipeline<*, *> {
+    lateinit var importValue: String
+    lateinit var importLocation: String
 
-        return state.copy(
-          errors = state.errors + CompileError(
-            message = "Too many arguments given to import statement",
-            tokenData = tokenData.unconsumed()
-          )
+    return Pipeline.create()
+      .thenExpectToken { (tokens) -> tokens.consumeFirstIfType(TokenType.IMPORT) }
+
+      .thenCheck { _, (tokens) -> tokens.unconsumed.size > 1 }
+      .ifTrueThenError("Too many arguments given to import statement")
+
+      .thenCheck { _, (tokens) -> tokens.isConsumed() }
+      .ifTrueThenError("Import location not specified")
+
+      .thenDo { _, (tokens), _ -> importValue = tokens.consumeFirst().value }
+
+      .thenCheck { _, _ -> importValue.first() != '"' || importValue.last() != '"' }
+      .ifTrueThenError("Import location must be specified in quotes")
+
+      .thenDo { _, _, _ -> importLocation = importValue.substring(1, importValue.length - 1) }
+
+      .thenCheck { state, _ -> state.currentFile!!.imports.any { it.location == importLocation } }
+      .ifTrueThenError { _, _, _ -> "Duplicate import \"${importLocation}\"" }
+
+      .thenSetState { state ->
+        val value = Import(
+          location = importLocation
+        )
+
+        val newFile = state.currentFile!!.copy(
+          imports = state.currentFile.imports + value
+        )
+
+        state.copy(
+          currentFile = newFile,
+          files = state.files.replace(state.currentFile, newFile),
+          imports = state.imports + value
         )
       }
-      if (tokenData.tokens.unconsumed.size == 1) {
-        tokenData.tokens.consumeAll()
-
-        return state.copy(
-          errors = state.errors + CompileError(
-            message = "Import location not specified",
-            tokenData = tokenData.unconsumed()
-          )
-        )
-      }
-
-      val importValue = tokenData.tokens.unconsumed[1].value
-
-      if (importValue.first() != '"' || importValue.last() != '"') {
-        tokenData.tokens.consumeAll()
-
-        return state.copy(
-          errors = state.errors + CompileError(
-            message = "Import location must be specified in quotes",
-            tokenData = tokenData.unconsumed()
-          )
-        )
-      }
-
-      val importLocation = importValue.substring(1, importValue.length - 1)
-      val value = Import(
-        location = importLocation
-      )
-
-      if (state.currentFile!!.imports.any { it.location == importLocation }) {
-        tokenData.tokens.consumeAll()
-
-        return state.copy(
-          errors = state.errors + CompileError(
-            message = "Duplicate import \"${importLocation}\"",
-            tokenData = tokenData.unconsumed()
-          )
-        )
-      }
-
-      val newFile = state.currentFile.copy(
-        imports = state.currentFile.imports + value
-      )
-
-      tokenData.tokens.consumeAll()
-
-      return state.copy(
-        currentFile = newFile,
-        files = state.files.replace(state.currentFile, newFile),
-        imports = state.imports + value
-      )
-    }
-
-    return state
   }
 }
